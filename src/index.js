2450200d6b2499851f7897baddb14b027eaef96c
Steve Jenkins <steve@skysteve.com>
2017-02-20 20:02:19 +0000

    fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const Alexa = require('alexa-sdk');
 
-exports.handler = function(event, context, callback){
-    const alexa = Alexa.handler(event, context);
+exports.handler = function main(event, context) { // optional callback
+  const alexa = Alexa.handler(event, context);
 
-    const handlers = {
-        'LaunchRequest': function () {
-            this.emit('HelloWorldIntent');
-        },
-        'HelloWorldIntent': function () {
-            this.emit(':ask', 'What would you like to do?', 'Please say that again?');
-        }
-    };
+  const handlers = {
+    LaunchRequest: function LaunchRequest() {
+      this.emit('HelloWorldIntent');
+    },
+    HelloWorldIntent: function HelloWorldIntent() {
+      this.emit(':ask', 'What would you like to do?', 'Please say that again?');
+    }
+  };
 
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
