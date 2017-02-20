e8b137e3279aaf426e452bdd3d379da56225a310
Steve Jenkins <steve@skysteve.com>
2017-02-20 21:04:11 +0000

    import over require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const Alexa = require('alexa-sdk');
+import Alexa from 'alexa-sdk';
 
 exports.handler = function main(event, context) { // optional callback
   const alexa = Alexa.handler(event, context);
+  alexa.appId = 'ALEXA_SKILL_ID';
 
   const handlers = {
     LaunchRequest: function LaunchRequest() {
